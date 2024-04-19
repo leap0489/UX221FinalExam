b1d4d6b064c52cbe0f340e20dbe39caa0ed5a6c8
leap0489 <leap0489@users.noreply.github.com>
2024-04-19 21:12:19 +0000

    deploy: 77271c79ca5e7685d08161ed5c33f831af46c0e9

---
+++ wp-content/themes/child-theme-for-exam/customElements.js
@@ -0,0 +1,15 @@
+class CopyrightYear extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();
+    }
+}
+
+customElements.define("x-year", CopyrightYear);
+
+class TwoSidedMarket extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `<a href="shop" class="btn">Browse our kits</a>&nbsp;<a href="partners" class="btn">Sell kits with us</a>`;
+    }
+}
+
+customElements.define("x-twosides", TwoSidedMarket);
